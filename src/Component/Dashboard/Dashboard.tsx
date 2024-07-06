6c79c23223026b48835b0fbd441e53d543e1cb67
GanapathyKlite <ganapathy@klitetechnology.com>
2024-07-06 17:46:48 +0530

    Ads newly add in Dashboard by ganapathy

--- src/Component/Dashboard/Dashboard.tsx
+++ src/Component/Dashboard/Dashboard.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./Dashboard.css";
 import DashboardHero from "./Component/DashboardHero";
 import Footer from "../Footer/Footer";
+import Ads from "../Ads/Ads";
 // import DashboardNavbar from "./Component/DashboardNavBar";
 
 const Dashboard: React.FC = () => {
   return (
     <>
       <DashboardHero />
+      <Ads/>
       <Footer />
     </>
   );
