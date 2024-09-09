51770007a6f917fe1e03495598a06846974388c5
nithiyasnthamizhan <myemail>
2024-09-09 16:20:46 +0530

    itenarary

--- src/Component/CarList/Component/CarList.tsx
+++ src/Component/CarList/Component/CarList.tsx
@@ -10,6 +10,12 @@ import { useNavigate } from "react-router-dom";
 import { FaIndianRupeeSign } from "react-icons/fa6";
 import axios from "axios";
 import { useAuth } from "../../Auth/AuthContext";
+import { Modal } from "antd";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import resultNotFount from "../../../Assets/recordNotFound.png";
 import "./CarHero.css";
 
@@ -270,6 +276,7 @@ const CarList: React.FC<CarListProps> = ({ cars,duration, km, start_date,pickup_
   const {authToken} = useAuth();
   const navigate = useNavigate();
   const [tripType, setTripType] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => { 
     const storedtripType = sessionStorage.getItem("tripType");
@@ -408,6 +415,33 @@ const CarList: React.FC<CarListProps> = ({ cars,duration, km, start_date,pickup_
     "4": { icon: <BiSolidCarGarage key="carrier" />, name: 'Carrier' },
     "5": { icon: <FaTv key="tv" />, name: 'TV' },
 };
+
+const handleOpenModal = () =>{
+  setIsModalOpen(true);
+}
+
+const itinerary = [
+  {
+      "day": "Day 1",
+      "from": 10,
+      "to": 93,
+      "from_name": "Pondicherry",
+      "to_name": "Yercaud",
+      "km": "236",
+      "duration": "4 hr 53 min",
+      "itinerary": "Morning pick-up from Pondicherry & drive to Yercaud Hotel. On arrival check into the hotel. fresh up then visiting places Emerald lake, Botanical garden, Kiliyur falls, Lady's Seat, Pagoda point, Sheoy temple, Local town hotel for overnight stay...\n\n"
+  },
+  {
+      "day": "Day 2",
+      "from": 93,
+      "to": 10,
+      "from_name": "Yercaud",
+      "to_name": "Pondicherry",
+      "km": "236",
+      "duration": "4 hr 53 min",
+      "itinerary": "Morning at leisure. After breakfast, Anna Park, Arthur's seat, Silk farm, Rose Garden, Bears cave & Bears Hill etc. The Yercaud boathouse provides an ample opportunity to enjoy the scenic beauty of the hilly terrain through boat riding which one shouldn't miss when visiting Yercaud (on direct pay basis) You can also do \"Dirt Biking\" in a place close to Grange Resorts. It is around 4 kms from the lake. In the evening come back and overnight stay at the hotel.\nAfter breakfast check out from the hotel and drive to Pondicherry for your onward destination with lots of beautiful memories.\n\n"
+  }
+]
   return (
     <div>
       <div className="d-flex flex-column gap-4 px-3 pb-5">
@@ -467,7 +501,7 @@ const CarList: React.FC<CarListProps> = ({ cars,duration, km, start_date,pickup_
                     {/* <div className="text-success pe-3">
                       <FaGasPump />
                     </div> */}
-                    <div className="d-flex col-11 col-md-8">
+                    <div className="d-flex col-12 col-md-8">
                       <div className="d-flex col-5 font-size14">Amenities</div>
                       {/* <div className="d-flex col-6 gap-3 align-items-center text-success">
                         {car.amenities.map((amenity) => amenitiesMap[amenity] || null)}
@@ -480,7 +514,12 @@ const CarList: React.FC<CarListProps> = ({ cars,duration, km, start_date,pickup_
         </div>
     ))}
 </div>
+  
                     </div>
+                    {tripType === "Holidays Package" ? (<div className="itinerary">
+   <span onClick={handleOpenModal
+    // ()=>{handleOpenModal(car.itinerary)}
+    }>itinerary</span>  </div>):null}
                   </div>
                 </div>
               </div>
@@ -616,6 +655,67 @@ const CarList: React.FC<CarListProps> = ({ cars,duration, km, start_date,pickup_
           </div>
         ))} */}
 
+
+<Modal
+        title={<p className="m-0">Itenerary</p>}
+        footer={null}
+        className="selectPaymentMode col-12 col-md-6"
+        // style={{ minWidth: "350", maxWidth: "550px" }}
+        open={isModalOpen}
+        onCancel={() => {
+          setIsModalOpen(false)}}
+      >
+        <div className="paymentDetailsDiv">    
+        <div className="left_side d-flex gap-3 flex-column">
+                {/* <div className="h5">Itinerary</div> */}
+                <div className="font-size14">Day Wise Details of your package</div>
+                <div>
+                {itinerary.map((item: any, index: number) => (<>
+
+                  <Accordion key={index}
+                  defaultExpanded={index === 0}
+                  disableGutters
+                  sx={{
+                    marginBottom: 0, 
+                    padding: 0, 
+                    boxShadow: 'none', 
+                  }}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls={`panel${index}-content`}
+            id={`panel${index}-header`}
+            sx={{ backgroundColor: 'lightgrey' }}
+          >
+            <Typography variant="subtitle1" 
+            >
+              {item.from_name} - {item.to_name}
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <div style={{ display: 'flex' }}>
+              <div 
+              style={{
+                width: '40%',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRight: '1px solid black', 
+              }}>
+                <Typography variant="body1"><strong>{item.day}</strong></Typography>
+                <Typography variant="body1"><strong>{item.duration}</strong></Typography>
+              </div>
+              <div style={{ width: '60%', padding: "10px" }}>
+                <Typography variant="body1">{item.itinerary}</Typography>
+              </div>
+            </div>
+          </AccordionDetails>
+        </Accordion>
+</>))}
+                </div>
+                  </div>
+        </div>
+         </Modal>
       </div>
     </div>
   );
